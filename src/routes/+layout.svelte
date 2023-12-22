2c31fc5f3c17c15b2833ac3b6ae731861954809d
ivynya <ivy@ivy.direct>
2023-12-22 15:13:07 -0800

    feat: global layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import "$lib/app.scss";
+	import { onMount } from "svelte";
+  import { stylesheet, initialize, isLight } from "@ivynya/leaf";
+
+  onMount(() => {
+    initialize();
+		stylesheet.subscribe(sheet => {
+			document.body.setAttribute("style", sheet);
+			document.body.setAttribute("data-dark", $isLight ? "false" : "true");
+		});
+  });
+</script>
+
+<main>
+  <slot />
+</main>
+
+<style lang="scss">
+  main {
+    padding: 30px;
+  }
+</style>
