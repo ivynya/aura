21ef3516c4a8de342b00a1824d4cda45205a9204
ivynya <ivy@ivy.direct>
2023-12-26 18:41:56 -0800

    feat: edit models

--- src/lib/remote.ts
+++ src/lib/remote.ts
@@ -25,11 +25,13 @@ export function mapModelShort(model: string): string {
   switch (model) {
     case "mistral":
       return "mistral:latest";
-    case "mistral:q2_k":
+    case "m:q2_k":
       return "mistral:7b-instruct-v0.2-q2_K";
+    case "m:orca":
+      return "mistral-openorca:latest";
     case "codellama":
       return "codellama:instruct";
-    case "codellama:13b":
+    case "c:13b":
       return "codellama:13b-instruct";
     default:
       return model;
